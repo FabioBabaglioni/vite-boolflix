fcfbb2347d6900695d2d3823633e61704c0c7f36
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-16 11:59:06 +0100

    Creo collegamenti store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,11 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-
+    apiUrl: `https://api.themoviedb.org/3`,
+    movies: [],
+    series: [],
+    apiUrlMovies: "/search/movie/550&query=",
+    apiUrlSeries: "/search/series/550&query=",
+    apiKey: "?api_key=fa8e15da4a5b52dca91a0f6a7d7abde2",
+    apiSearchText: "",
 })
