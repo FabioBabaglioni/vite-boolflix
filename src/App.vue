a7bdf6f3b5d3620a48295efeeb627605f64aa7bf
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-16 12:46:56 +0100

    Inserisco axios per la chiamata api

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
 <script >
 import appHeader from './components/appHeader.vue'
+import { store } from `./store`
 import appMain from './components/appMain.vue'
+
 
 
 export default {
   components: {
     appHeader, appMain
+  },
+
+  data() {
+    return {
+      store
+    }
+  },
+
+  methods: {
+    getCharacters() {
+
+      axios
+        .get(apiUrl += `${store.apiKey}${store.apiUrlMovies}${store.apiSearchText}`)
+        .then(res => {
+          store.movies = ;
+        })
+        .catch(err => {
+          console.log("Errori, err")
+        })
+    }
+  },
+  mounted() {
+    this.getCharacters()
   }
+}
 }
 </script>
 
