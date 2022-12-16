73307e1ea78f0fa7dd46fbd7f4ae6e97ba6403b8
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-16 12:19:17 +0100

    Creo input e button statici

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -7,9 +7,26 @@ export default {
 </script>
 
 <template>
+    <section>
+        <div class="d-flex justify-content-between align-items-center container_width">
+            <h1>BOOLFLIX</h1>
+            <label for="">
+                <input type="text" class="me-2" placeholder="Cerca film">
+                <button>search</button>
+            </label>
 
+        </div>
+
+    </section>
 </template>
 
 <style scoped>
+section {
+    height: 100px;
+    background-color: black;
+}
 
+h1 {
+    color: red;
+}
 </style>
