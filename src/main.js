5fa250f9acb428945cea051e9ce4dbe641103a68
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-20 11:20:54 +0100

    Inserisco fontawasome

--- src/main.js
+++ src/main.js
@@ -2,4 +2,13 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// fontawasome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons'
+
+
+library.add(fasStar, farStar)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
