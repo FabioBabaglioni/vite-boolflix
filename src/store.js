9f7980b934fa95d855c29aeee4a653b53b20d39c
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-12-20 15:48:59 +0100

    Refactoring stringhe inutilizzate

--- src/store.js
+++ src/store.js
@@ -1,11 +1,16 @@
 import { reactive } from "vue";
 
 export const store = reactive({
+
+    // url iniziale
     apiUrl: `https://api.themoviedb.org/3/search/multi`,
+
+    // array da popolare con la chiamata all'API
     movies: [],
-    series: [],
-    apiMovie: `movie?`,
-    apiSeries: `tv?`,
+
+    // api key per l'identificazione di chi sta facendo la chimata
     apiKey: `?api_key=e99307154c6dfb0b4750f6603256716d&query=`,
+
+    // stringa dove andrò a vedere con il v-model cosa scrive l'utente
     apiSearchText: "",
 })
